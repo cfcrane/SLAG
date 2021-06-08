3cf2f92968779d6ca6d0be76343f80a7bb795553
cfcrane <ccrane@purdue.edu>
2021-06-07 21:32:47 -0400

    Add files via upload

---
+++ rahTaisospa.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+source /etc/profile
+ml bioinfo aTRAM
+#atram.py --blast-db=path_to_atram_library/LIBRARY_PREFIX --query=path_to_reference_loci/Locus.fasta --assembler=ASSEMBLER_CHOICE --output-prefix=path_to_output/OUTPUT_PREFIX
+echo "atram.py --blast-db=/scratch/brown/ycrane/hemiSRR9125476_htsclepurrenrefforatram2 --query=Zeaisocitratedehydrogenases.fasta --assembler=spades --output-prefix=/scratch/brown/ycrane/atramout/hemiStanleyisocitratedehydrogenasesbyatram2 --iterations=21 --protein --cpus=10 --log-file=hemiStanleyisocitratedehydrogenasesbyatram2log.txt --temp-dir=/scratch/brown/ycrane/atramtemp --timeout=100000 --no-filter --evalue=1e-10 --max-memory=95"
+date
+atram.py --blast-db=/scratch/brown/ycrane/hemiSRR9125476_htsclepurrenrefforatram2 --query=Zeaisocitratedehydrogenases.fasta --assembler=spades --output-prefix=/scratch/brown/ycrane/atramout/hemiStanleyisocitratedehydrogenasesbyatram2 --iterations=21 --protein --cpus=10 --log-file=hemiStanleyisocitratedehydrogenasesbyatram2log.txt --temp-dir=/scratch/brown/ycrane/atramtemp --timeout=100000 --no-filter --evalue=1e-10 --max-memory=95
+date
+exit
